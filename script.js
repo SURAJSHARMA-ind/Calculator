0f60a3bc893c4918fcf4df10d169e291e43dd82c
Suraj Sharma <80053118+SURAJSHARMA-ind@users.noreply.github.com>
2023-04-22 23:59:06 +0530

    add back btn

--- script.js
+++ script.js
@@ -6,17 +6,23 @@ Array.from(buttons).forEach(function(a) {
   a.addEventListener("click", function() {
     try {
       if (a.innerHTML === "=") {
-        string = eval(string);
+        // here i convert string variable to string because after calculation it return number 
+        string = eval(string).toString();
         input.value = string;
       } else if (a.innerHTML == "AC") {
         string = "";
         input.value = string;
-      } else {
+      } else if(a.innerHTML === "&lt;" ){
+        string = string.slice(0,-1)
+        input.value=string
+        console.log("hey u call me")
+      }
+      else {
         string = string + a.innerHTML;
         input.value = string;
       }
     } catch (error) { 
-      alert("Math error ");
+      alert("Math error " + error);
     }
   });
 });
